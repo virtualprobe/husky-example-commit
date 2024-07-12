421eceb64329f0fe716dba505532757d9816afdf
tiziano <tiziano@Computeitiziano.station>
2024-07-12 15:32:18 +0200

    Breaking: added new endpoint for test purpose

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/test', (req, res) => {
+    res.send('versioning test!');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
