97e3f65889a809901055eb577e9a489489e6e91e
tiziano <tiziano@Computeitiziano.station>
2024-07-12 15:52:57 +0200

    Fix: added new endpoint for test purpose

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 app.get('/test', (req, res) => {
-    res.send('versioning test!');
+    res.send('versioning!');
 });
 
 app.listen(port, () => {
