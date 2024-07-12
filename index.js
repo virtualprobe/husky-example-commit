2be02854eb5c74c650a34d806e1e46fb282e2e16
tiziano <tiziano@Computeitiziano.station>
2024-07-12 15:53:15 +0200

    Feature: added new endpoint for test purpose

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 app.get('/test', (req, res) => {
-    res.send('versioning!');
+    res.send('hello test!');
 });
 
 app.listen(port, () => {
