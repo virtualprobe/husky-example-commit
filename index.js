c911f243d1b4bf526364810c32d58059046eb51a
TIZIANO MARCOZZI <t.marcozzi@outlook.com>
2024-07-15 11:43:00 +0200

    Feature: added endopoint for raffaele

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
     res.send('versioning test!');
 });
-app.get('/angelika', (req, res) => {
-    res.send('ma quanto è bella angelika!!!');
+app.get('/raffaele', (req, res) => {
+    res.send('ma quanto è bello raffaele!!!');
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
